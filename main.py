065a792ae71c47289cdaa1b43f4c03e14eac7a13
Matej Delincak <55759748+mateju25@users.noreply.github.com>
2020-11-11 10:13:02 +0100

    base gui added

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
-import tkinter as tk
-import sys
+from tkinter import *
 
-class PrintLogger(): # create file like object
-    def __init__(self, textbox): # pass reference to text widget
-        self.textbox = textbox # keep ref
+import pygubu
 
-    def write(self, text):
-        self.textbox.insert(tk.END, text) # write text to textbox
-            # could also scroll to end of textbox here to make sure always visible
 
-    def flush(self): # needed for file like object
+class GuiDataApp:
+    def __init__(self):
+        self.builder = builder = pygubu.Builder()
+        builder.add_from_file("gui_data.ui")
+        self.mainwindow = builder.get_object('frame_1')
+        builder.connect_callbacks(self)
+
+    def run(self):
+        self.mainwindow.mainloop()
+
+    def client_clicked(self):
+        radio = self.builder.objects.get("Server").widget
+        radio.var = False
+
+    def server_clicked(self):
+        radio = self.builder.objects.get("Client").widget
+        radio.var = True
+
+    def send_mess(self):
+        text = self.builder.objects.get("text_console").widget
+        text.insert(END, "ssss\n")
         pass
 
+
 if __name__ == '__main__':
-    def do_something():
-        print('i did something')
-        root.after(1000, do_something)
+    import tkinter as tk
 
     root = tk.Tk()
-    t = tk.Text()
-    t.pack()
-    # create instance of file like object
-    pl = PrintLogger(t)
+    app = GuiDataApp()
+    app.run()
 
-    # replace sys.stdout with our object
-    sys.stdout = pl
-
-    root.after(1000, do_something)
-    root.mainloop()
